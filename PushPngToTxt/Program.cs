340dab8bc320d797bcf78437e7fa6e2feef9c3d5
598224311@qq.com <dgt742cv>
2023-12-01 14:21:16 +0800

    pushpngTotxt

--- PushPngToTxt/Program.cs
+++ PushPngToTxt/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace PushPngToTxt
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PushPngToTxt
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void ProcessDirectory(string dir)
         {
+            var files = Directory.GetFiles(dir);
+            var dirs = Directory.GetDirectories(dir);
+            foreach (var subdir in dirs)
+                ProcessDirectory(subdir);
+            var loadList = new List<string>();
+
+            if (files.Length > 0)
+            {
+                foreach (var file in files)
+                {
+                    var path = Path.GetDirectoryName(file);
+                    var ext = Path.GetExtension(file);
+                    var relPath = file.Replace(@"..\Resources\", "").Replace(@"..\Resources", "").Replace("\\", "/");
+
+                    if (ext.Equals(".png"))
+                        loadList.Add(relPath);
+                }
+
+
+                string directory = dir;
+                string fileName = Path.GetFileNameWithoutExtension(directory);
+                string newDirectory = fileName + ".txt";
+
+                File.WriteAllLines(Path.Combine(@"..\Resources\image", newDirectory), loadList);
+
+                foreach (var png in loadList)
+                {
+                    Console.WriteLine(newDirectory + " : " + png);
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Resources");
+            ProcessDirectory(@"..\Resources\image");
 
         }
     }
 }
+
+
